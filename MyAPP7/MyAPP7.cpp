2065fce7b9f0cbac2192b198b309eb5a9a5c5650
lEONARNO23 <gfhju21s2@gmail.com>
2024-04-02 20:40:40 +0300

    VER.1 ~FIXED DIVIDE ON ZERO ---> ADDED CHECK

--- MyAPP7/MyAPP7.cpp
+++ MyAPP7/MyAPP7.cpp
@@ -5,10 +5,15 @@ int main() {
     int y = 0;
 
   
-    int z = x / y;
+    if (x || y == 0) 
+    {
+        std::cout << "devide on zero!! ERROR ";
+        return 0;
 
+    }; 
+    int z = x / y; //ver 1.0 исправлено деление на ноль, добавлена проверка
 
-    std::cout << "The result of division is: " << z << std::endl
+    std::cout << "The result of division is: " << z << std::endl 
 
       
         int uninitialized_value;
