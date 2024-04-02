8f6dad45641879f43927fc979c8da7fd8e9e5611
lEONARNO23 <gfhju21s2@gmail.com>
2024-04-02 20:06:13 +0300

    First ver.

---
+++ MyAPP7/MyAPP7.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int main() {
+    int x = 5;
+    int y = 0;
+
+  
+    int z = x / y;
+
+
+    std::cout << "The result of division is: " << z << std::endl
+
+      
+        int uninitialized_value;
+    std::cout << "Uninitialized value: " << uninitialized_value << std::endl;
+
+
+    if (x = 0) {
+        std::cout << "x is zero." << std::endl;
+    }
+
+    for (int i = 0; i < 5; ++i)
+        std::cout << i << " ";
+
+    return 0;
+}
